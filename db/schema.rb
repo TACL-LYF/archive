ef2326651a2a83af10997a89c27516e90ca3e83c
Brian Lin <me@brianl.in>
2016-11-05 13:13:17 -0700

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105195219) do
+ActiveRecord::Schema.define(version: 20161105200157) do
 
   create_table "camps", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,24 @@ ActiveRecord::Schema.define(version: 20161105195219) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["year"], name: "index_camps_on_year", unique: true
+  end
+
+  create_table "families", force: :cascade do |t|
+    t.string   "primary_parent_first_name"
+    t.string   "primary_parent_last_name"
+    t.string   "primary_parent_email"
+    t.string   "primary_parent_phone_number"
+    t.string   "secondary_parent_first_name"
+    t.string   "secondary_parent_last_name"
+    t.string   "secondary_parent_email"
+    t.string   "secondary_parent_phone_number"
+    t.string   "suite"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
 end
