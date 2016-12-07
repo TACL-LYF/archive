159ea1415cb1b0be00947261d8ae5447f4ef751a
Rosette Su <su.rosette@gmail.com>
2016-12-06 23:05:49 -0800

    Don't add registration discounts or previous camps in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# make some referral methods
+# make referral methods
 detailed = ["TACL-LYF Counselors", "TACL-LYF Campers and Families",
   "Other Taiwanese Organizations", "Other"]
 ["TACL-LYF Counselors", "TACL-LYF Campers and Families", "JTASA", "TAP",
@@ -7,15 +7,6 @@ detailed = ["TACL-LYF Counselors", "TACL-LYF Campers and Families",
   ReferralMethod.create!(name: method, allow_details: detailed.include?(method))
 end
 
-# make some camps
-Camp.create!(year: 2016, name: "When LYF Gives You Lemons",
-  registration_fee: 600, sibling_discount: 40, shirt_price: 15)
-Camp.create!(year: 2015, name: "Circle of LYF",
-  registration_fee: 500, sibling_discount: 40, shirt_price: 15)
-camp = Camp.create!(year: 2017, name: "To Be Determined",
+# make current camp
+Camp.create!(year: 2017, name: "To Be Determined",
   registration_fee: 680, sibling_discount: 40, shirt_price: 15)
-
-# make some discounts
-camp.registration_discounts.create!(code: "ANNIE", discount_percent: 20)
-camp.registration_discounts.create!(code: "BRIAN", discount_percent: 10)
-camp.registration_discounts.create!(code: "EMILY", discount_percent: 15)
