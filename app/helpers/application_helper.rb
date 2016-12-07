61377f77ca0ed00e8aa3aad69edc2bb0f4ec6854
Rosette Su <su.rosette@gmail.com>
2016-12-07 14:52:19 -0800

    Use main LYF site to serve VAGRundschrift font

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-  def reg_title(step_title = '')
-    base_title = 'LYF Camp 2017 Registration'
-    base_title.empty? ? base_title : "#{step_title} | #{base_title}"
-  end
 end
