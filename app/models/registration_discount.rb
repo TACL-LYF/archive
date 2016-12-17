99b726c715dfdb535a2656874ad0f0c477147c7d
Rosette Su <su.rosette@gmail.com>
2016-12-17 01:08:46 -0800

    Add financial aid modal

--- app/models/registration_discount.rb
+++ app/models/registration_discount.rb
@@ -2,15 +2,15 @@ class RegistrationDiscount < ApplicationRecord
   belongs_to :camp, inverse_of: :registration_discounts
   belongs_to :registration_payment, inverse_of: :registration_discount, optional: true
 
-  before_save { self.code = code.upcase }
-  before_save :toggle_redeem_if_payment_exists
-
   validates :code, :discount_percent, presence: true
   validates :code, uniqueness: { scope: :camp_id }
   validates :redeemed, inclusion: { in: [true, false] }
   validates :discount_percent, numericality: { only_integer: true },
                                inclusion: { in: 1..100 }
   validates :registration_payment_id, presence: true, if: :redeemed
+
+  before_validation { self.code = code.gsub(/\s+/, '').upcase }
+  before_save :toggle_redeem_if_payment_exists
 
   def self.get(code)
     where(code: normalize_code(code), redeemed: false).take
