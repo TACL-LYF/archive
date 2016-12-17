0091e394883cf1231f19803b7134ce8425a66660
Rosette Su <su.rosette@gmail.com>
2016-12-16 16:34:07 -0800

    Make details field label required if details allowed

--- app/models/referral_method.rb
+++ app/models/referral_method.rb
@@ -2,5 +2,6 @@ class ReferralMethod < ApplicationRecord
   has_many :referrals, inverse_of: :referral_method
   has_many :families, through: :referrals
   validates :name, presence: true
+  validates :details_field_label, presence: true, if: :allow_details
   validates_inclusion_of :allow_details, in: [true, false]
 end
