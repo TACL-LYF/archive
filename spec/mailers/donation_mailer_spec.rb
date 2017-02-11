1340a8b31c92e1f334e15a821b39fc6a3da88c28
Rosette Su <su.rosette@gmail.com>
2017-02-10 18:43:59 -0800

    Mailer tests

--- spec/mailers/donation_mailer_spec.rb
+++ spec/mailers/donation_mailer_spec.rb
@@ -1,17 +1,38 @@
 require "rails_helper"
 
-RSpec.describe DonationMailerMailer, type: :mailer do
+RSpec.describe DonationMailer, type: :mailer do
   describe "donation_confirmation" do
-    let(:mail) { DonationMailerMailer.donation_confirmation }
+    let(:donation) { build(:donation, amount: "50.00") }
+    let(:mail) { DonationMailer.donation_confirmation(donation) }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Donation confirmation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+    it "renders the subject" do
+      expect(mail.subject).to eq("TACL-LYF Donation Confirmation")
+    end
+    it "renders the receiver email" do
+      expect(mail.to).to eq(["#{donation.email}"])
+    end
+    it "renders the sender email" do
+      expect(mail.from).to eq(["lyf@tacl.org"])
+    end
+    it "renders the bcc email" do
+      expect(mail.bcc).to eq(["lyf.treasurer@tacl.org"])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "renders the donation amount" do
+      expect(mail.body.encoded).to match(donation.amount.to_s)
+    end
+
+    it "does not include text about a donation receipt" do
+      expect(mail.body.encoded)
+        .to_not match("donation receipt for charitable tax deductions")
+    end
+
+    context "when the donation amount > 100" do
+      let (:donation) { build(:donation, amount: "100.00") }
+      it "includes text about a donation receipt" do
+        expect(mail.body.encoded)
+          .to match("donation receipt for charitable tax deductions")
+      end
     end
   end
 
--- spec/mailers/registration_payment_mailer_spec.rb
+++ spec/mailers/registration_payment_mailer_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 
 RSpec.describe RegistrationPaymentMailer, type: :mailer do
   describe "registration_confirmation" do
-    let(:mail) { RegistrationPaymentMailerMailer.registration_confirmation }
+    let(:reg_payment) { build(:registration_payment) }
+    let(:mail) { RegistrationPaymentMailer.registration_confirmation(reg_payment) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Registration confirmation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("LYF Camp 2017 Registration Confirmation")
+      expect(mail.to).to eq(["#{reg_payment.registrations.first.camper.family.primary_parent_email}"])
+      expect(mail.from).to eq(["lyf@tacl.org"])
+      expect(mail.bcc).to eq(["lyf.od@tacl.org"])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
-    end
+    it "renders the body"
   end
 
 end
