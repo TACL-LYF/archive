52f47169d4f4cadc4954073cb8babd429979c295
Rosette Su <su.rosette@gmail.com>
2017-07-20 16:29:30 -0700

    Email on exception

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,11 @@ Rails.application.configure do
 
   # Configure Exception Notification gem
   Rails.application.config.middleware.use ExceptionNotification::Rack,
+    :email => {
+      :email_prefix => "[LYF-ARCHIVE ERROR]",
+      :sender_address => %{"notifier" <lyf@tacl.org>},
+      :exception_recipients => %w{su.rosette@gmail.com}
+    },
     :slack => {
       :webhook_url => ENV['SLACK_WEBHOOK_URL'],
       :backtrace_lines => 5
