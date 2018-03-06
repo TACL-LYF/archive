4bfc6021affdb161ea45110a73923766b5357361
Rosette Su <su.rosette@gmail.com>
2018-03-06 15:49:51 -0800

    Remove destroy dependency from referral belongs_to referral_method

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,6 +1,6 @@
 class Referral < ApplicationRecord
   belongs_to :family, inverse_of: :referrals, dependent: :destroy
-  belongs_to :referral_method, inverse_of: :referrals, dependent: :destroy
+  belongs_to :referral_method, inverse_of: :referrals
 
   validates :family, :referral_method, presence: true
 end
