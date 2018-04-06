00774e448b1e200d27097caf666da0be08e8061b
Rosette Su <su.rosette@gmail.com>
2018-04-05 22:33:02 -0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   resources :donations, only: [:new], except: [:create] do
     # post "create" => "donations#create", as: :create, path: :new, on: :collection
-    post 'create', to: 'donations#create', on: :collection
+    post 'create', to: 'donations#create', as: :create, on: :collection
   end
   get '/donations/confirm', to: 'donations#confirm', as: :donation_confirmation
 
   resources :last_day_purchases, path: '/lastdaypurchases', only: [:new], except: [:create] do
     # post "create" => "last_day_purchases#create", as: :create, path: :new, on: :collection
-    post 'create', to: 'last_day_purchases#create', on: :collection
+    post 'create', to: 'last_day_purchases#create', as: :create, on: :collection
   end
   get '/lastdaypurchases/confirm', to: 'last_day_purchases#confirm', as: :last_day_purchase_confirmation
   get '/lastdaypurchases', to: redirect('/lastdaypurchases/new')
