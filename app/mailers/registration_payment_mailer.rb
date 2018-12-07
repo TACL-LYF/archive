aacc73a896f02c18e6350e44d113bdb2252ee66e
Rosette Su <su.rosette@gmail.com>
2018-12-06 17:16:44 -0800

    Include registration info in confirmation email; update contact emails on form

--- app/mailers/registration_payment_mailer.rb
+++ app/mailers/registration_payment_mailer.rb
@@ -6,12 +6,14 @@ class RegistrationPaymentMailer < ApplicationMailer
   #   en.registration_payment_mailer.registration_confirmation.subject
   #
   def registration_confirmation(reg_payment)
+    @registrations = reg_payment.registrations
+    @family = @registrations.first.camper.family
     @camp = Camp.first
     @breakdown = reg_payment.breakdown
     @last_four = reg_payment.stripe_last_four
     @brand = reg_payment.stripe_brand
     @is_waitlist = reg_payment.registrations.first.waitlist?
-    mail to: reg_payment.registrations.first.camper.family.primary_parent_email,
+    mail to: @family.primary_parent_email,
          bcc: "lyf.od@tacl.org",
          subject: "LYF Camp #{@camp.year} Registration Confirmation"
   end
--- spec/mailers/previews/registration_payment_mailer_preview.rb
+++ spec/mailers/previews/registration_payment_mailer_preview.rb
@@ -4,7 +4,7 @@ class RegistrationPaymentMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://localhost:3000/rails/mailers/registration_payment_mailer/registration_confirmation
   def registration_confirmation
-    reg_payment = RegistrationPayment.first
+    reg_payment = RegistrationPayment.find(92)
     RegistrationPaymentMailer.registration_confirmation(reg_payment)
   end
 end
