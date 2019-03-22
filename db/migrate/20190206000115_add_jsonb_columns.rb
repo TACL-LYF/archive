63f45ea49bb24a69ed15c8990bb7023623d07d8c
Rosette Su <su.rosette@gmail.com>
2019-03-21 17:58:11 -0700

    Fixed and merged migrations

--- db/migrate/20190206000115_add_jsonb_columns.rb
+++ db/migrate/20190206000115_add_jsonb_columns.rb
@@ -1,34 +1,50 @@
 class AddJsonbColumns < ActiveRecord::Migration[5.1]
   def up
-    add_column :registrations, :additional_shirts_json, :jsonb, null: false, default: {}
-    add_column :registrations, :camper_involvement_json, :jsonb, null: false, default: {}
+    # rename old text columns
+    rename_column :registrations, :additional_shirts, :additional_shirts_old
+    rename_column :registrations, :camper_involvement, :camper_involvement_old
 
-    add_index  :registrations, :additional_shirts_json, using: :gin
-    add_index  :registrations, :camper_involvement_json, using: :gin
+    # add new jsonb columns and indices
+    add_column :registrations, :additional_shirts, :jsonb, null: false, default: {}
+    add_column :registrations, :camper_involvement, :jsonb, null: false, default: {}
+    add_index  :registrations, :additional_shirts, using: :gin
+    add_index  :registrations, :camper_involvement, using: :gin
 
-    # copy additional_shirts to additional_shirts_json
-    empty_as = {"x_small"=>"", "small"=>"", "medium"=>"", "large"=>"", "x_large"=>"", "xx_large"=>""}
-    Registration.where(additional_shirts: empty_as).each do |r|
-      r.update_attribute("additional_shirts", {})
+    def stringToHash(str)
+      s = str.sub("--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\n", "")
+      s = s.split("\n").map!{|x| x.split(": ")}
+      h = Hash.new
+      s.each{|pair| h[pair.first] = pair.last.gsub("'", "")}
+      return h
     end
 
-    Registration.where.not(additional_shirts: nil).each do |r|
-      r.update_attribute("additional_shirts_json", JSON.parse(r.additional_shirts.to_json))
+    # find "empty" additional_shirts_old fields and replace with nil
+    empty_as = "--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nx_small: ''\nsmall: ''\nmedium: ''\nlarge: ''\nx_large: ''\nxx_large: ''\n"
+    Registration.where(additional_shirts_old: empty_as).each do |r|
+      r.update_attribute("additional_shirts_old", nil)
     end
 
-    # copy camper_involvement to camper_involvement_json
-    empty_ci = {"clinic"=>"", "large_group_icebreaker"=>"", "family_activity"=>"", "night_market_booth"=>"", "bus_monitor"=>""}
-    Registration.where(camper_involvement: empty_ci).each do |r|
-      r.update_attribute("camper_involvement", {})
+    # copy nonempty additional_shirts_old to new jsonb column
+    Registration.where.not(additional_shirts_old: nil).each do |r|
+      r.update_attribute("additional_shirts", stringToHash(r.additional_shirts_old))
     end
 
-    Registration.where.not(camper_involvement: nil).each do |r|
-      r.update_attribute("camper_involvement_json", JSON.parse(r.camper_involvement.to_json))
+    # find "empty" camper_involvement_old fields and replace with nil
+    empty_ci = "--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nclinic: ''\nlarge_group_icebreaker: ''\nfamily_activity: ''\nnight_market_booth: ''\nbus_monitor: ''\n"
+    Registration.where(camper_involvement_old: empty_ci).each do |r|
+      r.update_attribute("camper_involvement_old", nil)
+    end
+
+    # copy nonempty camper_involvement_old to new jsonb column
+    Registration.where.not(camper_involvement_old: nil).each do |r|
+      r.update_attribute("camper_involvement", stringToHash(r.camper_involvement_old))
     end
   end
 
   def down
-    remove_column :registrations, :additional_shirts_json, :jsonb
-    remove_column :registrations, :camper_involvement_json, :jsonb
+    remove_column :registrations, :additional_shirts, :jsonb
+    remove_column :registrations, :camper_involvement, :jsonb
+    rename_column :registrations, :additional_shirts_old, :additional_shirts
+    rename_column :registrations, :camper_involvement_old, :camper_involvement
   end
 end
