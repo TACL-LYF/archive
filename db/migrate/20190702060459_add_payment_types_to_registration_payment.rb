15c66d91b625af48fca350bcfa28f36650d2ccf5
Rosette Su <su.rosette@gmail.com>
2019-07-14 17:03:43 -0700

    Fix payment type migration to set existing payments' type to card

--- db/migrate/20190702060459_add_payment_types_to_registration_payment.rb
+++ db/migrate/20190702060459_add_payment_types_to_registration_payment.rb
@@ -1,7 +1,7 @@
 class AddPaymentTypesToRegistrationPayment < ActiveRecord::Migration[5.1]
   def up
     add_column :registration_payments, :payment_type, :integer, default: 0
-    RegistrationPayment.all.each{ |rp| rp.update_columns(payment_type: 0)}
+    RegistrationPayment.all.each{ |rp| rp.update_columns(payment_type: 1)}
     add_column :registration_payments, :check_number, :string
   end
 
